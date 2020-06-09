8286094cada71c00ff3c9b1101b8e6d5939ac22d
Stewart ilondanga <stilondanga@gmail.com>
2020-06-09 11:33:40 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,27 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var switches = document.querySelectorAll(".switch__input");
+
+var getRandom = function(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+for (var i = 0; i < switches.length; i++) {
+  var sw = switches[i];
+
+  sw.onchange = function(e) {
+    var activeSwitches = Array.prototype.slice.call(
+      document.querySelectorAll(":checked")
+    );
+
+    if (activeSwitches.length > 2) {
+      var notSelf = activeSwitches.filter(function(item) {
+        return item !== e.target;
+      });
+
+      notSelf[getRandom(0, 1)].checked = false;
+    }
+  }
+}
